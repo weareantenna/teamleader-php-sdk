8a33894d36b1d3d483f15cd5442d21e0117afc55
Enzo <enzosimoen@gmail.com>
2020-03-06 16:39:04 +0100

    Abstract ID

--- src/Models/CompanyId.php
+++ src/Models/CompanyId.php
@@ -4,37 +4,10 @@ declare(strict_types=1);
 
 namespace Antenna\TeamleaderSDK\Models;
 
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
-use Teamleader\Uuidifier\Uuidifier;
-
-final class CompanyId
+final class CompanyId extends Id
 {
-    /** @var UuidInterface */
-    private $uuid;
-
-    private function __construct(UuidInterface $uuid)
+    protected static function getPrefix(): string
     {
-        $this->uuid = $uuid;
-    }
-
-    public static function fromV1(int $id) : self
-    {
-        return new self((new Uuidifier())->encode('CompanyId', $id));
-    }
-
-    public static function fromV2(string $id) : self
-    {
-        return new self(Uuid::fromString($id));
-    }
-
-    public function toV1() : int
-    {
-        return (new Uuidifier())->decode($this->uuid);
-    }
-
-    public function toV2() : string
-    {
-        return $this->uuid->toString();
+        return 'CompanyId';
     }
 }
---
+++ src/Models/Id.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Antenna\TeamleaderSDK\Models;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Teamleader\Uuidifier\Uuidifier;
+
+abstract class Id
+{
+    /** @var UuidInterface */
+    private $uuid;
+
+    private function __construct(UuidInterface $uuid)
+    {
+        $this->uuid = $uuid;
+    }
+
+    abstract protected static function getPrefix() : string;
+
+    public static function fromV1(int $id) : self
+    {
+        return new static((new Uuidifier())->encode(static::getPrefix(), $id));
+    }
+
+    public static function fromV2(string $id) : self
+    {
+        return new static(Uuid::fromString($id));
+    }
+
+    public function toV1() : int
+    {
+        return (new Uuidifier())->decode($this->uuid);
+    }
+
+    public function toV2() : string
+    {
+        return $this->uuid->toString();
+    }
+}
